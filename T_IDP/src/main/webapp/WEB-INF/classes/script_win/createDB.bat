33cce55117f9eb1be37173a5eea4631bdbbf6203
UnicusStella <UnicusStella@gmail.com>
2023-02-13 10:23:23 +0900

    Fix: Jcaos 버 업이트

--- T_IDP/src/main/webapp/WEB-INF/classes/script_win/createDB.bat
+++ T_IDP/src/main/webapp/WEB-INF/classes/script_win/createDB.bat
@@ -5,7 +5,7 @@ set CAT_PATH=C:\Users\stella\Desktop\Directory\(WAS)_Tomcat\lib
 set SSO_PATH=C:\Users\stella\Desktop\Sample\T_IDP\src\main\webapp\WEB-INF\lib
 set SSS=C:\Users\stella\Desktop\Sample\T_IDP\build\classes
 
-%JAV_PATH%\java -cp %SSS%;%SSO_PATH%\tibero6-jdbc.jar;%SSO_PATH%\mssql-jdbc-7.4.1.jre8.jar;%SSO_PATH%\mysql-connector-java-5.1.49.jar;%SSO_PATH%\postgresql-42.2.18.jre6.jar;%SSO_PATH%\JDBC-9.3.0.0206-cubrid.jar;%SSO_PATH%\MagicJCrypto-v2.0.0.0.jar;%SSO_PATH%\jcaos-1.4.9.6.jar;%SSO_PATH%\magicsso-server-4.0.0.3.jar;%SSO_PATH%\magicsso-svadd-4.0.0.3.jar;%CAT_PATH%\servlet-api.jar -Dfile.encoding=UTF-8 com.dreamsecurity.sso.server.config.CreateDB
+%JAV_PATH%\java -cp %SSS%;%SSO_PATH%\tibero6-jdbc.jar;%SSO_PATH%\mssql-jdbc-7.4.1.jre8.jar;%SSO_PATH%\mysql-connector-java-5.1.49.jar;%SSO_PATH%\postgresql-42.2.18.jre6.jar;%SSO_PATH%\JDBC-9.3.0.0206-cubrid.jar;%SSO_PATH%\MagicJCrypto-v2.0.0.0.jar;%SSO_PATH%\jcaos-arcCert-1.5.3.5.jar;%SSO_PATH%\magicsso-server-4.0.0.3.jar;%SSO_PATH%\magicsso-svadd-4.0.0.3.jar;%CAT_PATH%\servlet-api.jar -Dfile.encoding=UTF-8 com.dreamsecurity.sso.server.config.CreateDB
 
 set JAV_PATH=
 set CAT_PATH=
--- T_SP2/src/main/webapp/WEB-INF/dreamsso/script_win/initSSO.bat
+++ T_SP2/src/main/webapp/WEB-INF/dreamsso/script_win/initSSO.bat
@@ -1,11 +1,11 @@
 @echo off
 
-set JAV_PATH="C:\Program Files\Java\jre1.8.0_361\bin"
+set JAV_PATH="C:\Program Files\Java\jre1.8.0_351\bin"
 set CAT_PATH=C:\Users\stella\Desktop\Directory\(WAS)_Tomcat\lib
 set SSO_PATH=C:\Users\stella\Desktop\Sample\T_SP2\src\main\webapp\WEB-INF\lib
 set SSS=C:\Users\stella\Desktop\Sample\T_SP2\build\classes
 
-%JAV_PATH%\java -cp %SSS%;%SSO_PATH%\MagicJCrypto-v2.0.0.0.jar;%SSO_PATH%\jcaos-1.4.9.6.jar;%SSO_PATH%\magicsso-agent-4.0.0.3.jar;%SSO_PATH%\magicsso-agadd-4.0.0.3.jar;%CAT_PATH%\servlet-api.jar -Dfile.encoding=UTF-8 com.dreamsecurity.sso.agent.config.InitSSO
+%JAV_PATH%\java -cp %SSS%;%SSO_PATH%\MagicJCrypto-v2.0.0.0.jar;%SSO_PATH%\jcaos-arcCert-1.5.3.5.jar;%SSO_PATH%\magicsso-agent-4.0.0.3.jar;%SSO_PATH%\magicsso-agadd-4.0.0.3.jar;%CAT_PATH%\servlet-api.jar -Dfile.encoding=UTF-8 com.dreamsecurity.sso.agent.config.InitSSO
 
 set JAV_PATH=
 set CAT_PATH=
